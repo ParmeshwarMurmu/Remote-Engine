bfbf506cad0a570b33b242715dbe627878158e77
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-12-17 12:49:16 +0530

    pp

--- frontend/src/Variables/AllVariables.js
+++ frontend/src/Variables/AllVariables.js
@@ -1,9 +1,7 @@
 
-export const APP_URL = "https://remote-engine.onrender.com";
+export const APP_URL = "https://engine-1hin.onrender.com";
+// export const APP_URL = "https://remote-engine.onrender.com";
 export const TOKEN = localStorage.getItem('Remote-Engine-token');
 export const USER_ID = localStorage.getItem('Remote-Engine-userId');
 
 
-export const HEADERS = {
-    Authorization: `bearer ${TOKEN}`,
-};
